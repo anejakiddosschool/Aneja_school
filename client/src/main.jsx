12e64ce2947cf0bf9162d8626a1be5382d45cd71
Ajay <Ajay@ohlolocal.net>
2025-09-26 13:38:50 +0530

    updates # routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+
 import { NotificationProvider } from './context/NotificationContext';
 import './index.css';
 import App from './App';
@@ -8,10 +10,12 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    
+      <HashRouter>
       <NotificationProvider>
         <App />
       </NotificationProvider>
-    </Router>
+      </HashRouter>
+   
   </React.StrictMode>
 );
